e9bd4b91ed1320a785d2ae33a0838b001c4e040f
murilobezs <givewaybymf@gmail.com>
2024-08-08 09:06:23 -0300

    att

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Banner/>
+      <h1>ETEC MCM</h1>
+      <p>Teste de React</p>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
