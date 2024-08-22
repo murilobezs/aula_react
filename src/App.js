86848c76b51142ae5f959c78a59590caeada8444
murilobezs <givewaybymf@gmail.com>
2024-08-22 09:09:30 -0300

    aula 06 | ta na metade

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
+import Category from "./components/Category"
 import videos from "./json/videos.json"
+
+const categories = [
+  "Videoclipes",
+  "73 questões | Vogue",
+  "Musicais",
+  "Esporte",
+  "Entrevistas"
+]
+
+
+function filterCategory(id){
+  return videos.filter( video => video.category === categories[id] )
+}
 
 function App() {
   return (
@@ -11,17 +25,21 @@ function App() {
       <Header/>
       <Banner image="home"/>
       <Container>
-      <h2>Videoclipes</h2>
-        <section className="cards">
-        
-        {
-          videos.map((video) => {
-            return <Card id={video.id} key={video.id} />
-          })
-        }
-
-
-        </section>
+          <Category category="Videoclipes">
+            { filterCategory(0).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category> 
+          <Category category="73 questões | Vogue">
+            { filterCategory(1).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category> 
+          <Category category="Musicais">
+            { filterCategory(2).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category> 
+          <Category category="Esporte">
+            { filterCategory(3).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category> 
+          <Category category="Entrevistas">
+            { filterCategory(4).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category> 
       </Container>
       <Footer/>
     </>
