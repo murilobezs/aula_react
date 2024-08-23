12ea6be0084c1e46172f8baab90cd0e2f9acdd4e
murilobezs <givewaybymf@gmail.com>
2024-08-22 21:17:55 -0300

    aula 07 | feita

--- src/App.js
+++ src/App.js
@@ -3,43 +3,23 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
-import Category from "./components/Category"
-import videos from "./json/videos.json"
+import Category, {categories, filterCategory} from "./components/Category"
 
-const categories = [
-  "Videoclipes",
-  "73 questões | Vogue",
-  "Musicais",
-  "Esporte",
-  "Entrevistas"
-]
-
-
-function filterCategory(id){
-  return videos.filter( video => video.category === categories[id] )
-}
 
 function App() {
+  
   return (
     <>
       <Header/>
       <Banner image="home"/>
       <Container>
-          <Category category="Videoclipes">
-            { filterCategory(0).map((video) => <Card id={video.id} key={video.id} />)}
-          </Category> 
-          <Category category="73 questões | Vogue">
-            { filterCategory(1).map((video) => <Card id={video.id} key={video.id} />)}
-          </Category> 
-          <Category category="Musicais">
-            { filterCategory(2).map((video) => <Card id={video.id} key={video.id} />)}
-          </Category> 
-          <Category category="Esporte">
-            { filterCategory(3).map((video) => <Card id={video.id} key={video.id} />)}
-          </Category> 
-          <Category category="Entrevistas">
-            { filterCategory(4).map((video) => <Card id={video.id} key={video.id} />)}
-          </Category> 
+          
+          {categories.map((category, index) =>
+              <Category category={category}>
+                {filterCategory(index).map((video) => <Card id={video.id} key=
+                {video.id} />)}
+              </Category> 
+            )}  
       </Container>
       <Footer/>
     </>
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,8 +1,22 @@
+import videos from "../../json/videos.json"
 import styles from "./Category.module.css"
+
+export const categories = [
+    "Videoclipes",
+    "73 questões | Vogue",
+    "Musicais",
+    "Esporte",
+    "Entrevistas"
+]
+
+
+export function filterCategory(id){
+    return videos.filter( video => video.category === categories[id] )
+}
 
 function Category ({ category, children}){
     return(
-       <section className={styles.category}>
+        <section className={styles.category}>
             <h2>{category}</h2>
             <div>
                 { children }
